f814c69a035e9559dc074276e2dc284e7a77394e
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-08-03 02:28:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var ranked=document.getElementById('app').querySelectorAll('ul.ranked-list li')
  for (let i=0; i< ranked.length; i++) {
-   parseInt(ranked[i].innerHTML)+n
+   ranked[i].innerHTML=parseInt(ranked[i].innerHTML)+n
   }
   return ranked
 }
 function deepestChild() {
   var child=document.getElementById('#grand-node')
     for (let i=0; i< child.length; i++) {
-    child[i].innerHTML=(i+1).toString()
+    child[i].innerHTML=document.querySelector(child[i].innerHTML)
    }
-  return child[4]
+  return child
 }
