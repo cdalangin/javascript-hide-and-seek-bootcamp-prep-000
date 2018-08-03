f8934376f21058effecf2f24def609b9077bc0a8
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-08-03 02:48:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function increaseRankBy(n) {
   return ranked
 }
 function deepestChild() {
-  var child=document.querySelector('div#grand-node').querySelector('div'[3])
+  var child=document.getElementById('grand-node').querySelector('div'[3])
   return child
 }
