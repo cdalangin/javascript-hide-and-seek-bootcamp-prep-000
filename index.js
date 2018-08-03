5843f6d7a4e1ea37cb8c62281f2637f789bd1a16
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-08-03 02:50:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+function getFirstSelector(selector) {
+  var element=document.querySelector(selector)
+  return element
+}
+function nestedTarget() {
+ var nest=document.querySelector('#nested .target')
+ return nest
+}
+function increaseRankBy(n) {
+  var ranked=document.getElementById('app').querySelectorAll('ul.ranked-list li')
+ for (let i=0; i< ranked.length; i++) {
+   ranked[i].innerHTML=parseInt(ranked[i].innerHTML)+n
+  }
+  return ranked
+}
+function deepestChild() {
+  var child=document.querySelector('#grand-node div div div div')
+  return child
+}
