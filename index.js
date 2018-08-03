7878e749ab4314efad1ec2e22bd6c489580bbb62
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-08-03 01:57:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-
+function getFirstSelector(selector) {
+  var element=document.querySelector(selector)
+  return element
+}
+function nestedTarget() {
+ var nest=document.querySelector('#nested .target')
+ return nest
+}
+function increaseRankBy(n) {
+  var ranked=document.getElementById('app').querySelectorAll('ul.ranked-list li')
+ for (let i=0; i< ranked.length; i++) {
+   parseInt(ranked[i].innerHTML)+n
+  }
+  return ranked
+}
+function deepestChild() {
+  var child=document.querySelector('#grand-node')
+   for (let i=0; i< child.length; i++) {
+    child[i].innerHTML = 
+    querySelector('div')
+   }
+  return child
+  
+}
